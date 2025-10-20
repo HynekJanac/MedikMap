cea54917a253b4816022df2d559711501c4165d9
Hynek Janáč <hynekjanac@gmail.com>
2025-10-20 16:24:23 +0200

    Update Zdroje.js

--- Assets/Js/Content/Zdroje.js
+++ Assets/Js/Content/Zdroje.js
@@ -7,7 +7,7 @@ let Zdroje = [
   "nazev": "PropdeutikA – LF2",
   "odkaz": "http://new.propedeutika.cz/",
   "popis": "Výukové materiály k propedeutice vnitřního lékařství z 2. Lékařské faukulty, obsahují články k tématům chorobobips, fyzikální vyšetření atd. ",
-  "předmět": "Interní propedetuika 🩺",
+  "předmět": "Interní propedeutika 🩺",
   "jazyk": ["Čeština"],
   "ročník": [3],
   "typ": "Výukové materiály"
